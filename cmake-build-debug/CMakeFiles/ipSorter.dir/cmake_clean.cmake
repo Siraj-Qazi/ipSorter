8129ce14954ee45e6f52059c5a680e7cf4c5b173
Siraj-Qazi <fullbridgerectifier@gmail.com>
2020-01-12 23:09:30 +0500

    cmake debug files

---
+++ cmake-build-debug/CMakeFiles/ipSorter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ipSorter.dir/main.c.o"
+  "ipSorter"
+  "ipSorter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ipSorter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
